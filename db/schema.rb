80115ca0d1da3332f95b46d2fe26bb3cc6fe259f
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2024-11-26 21:05:49 -0500

    add products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_26_021034) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_27_011921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_26_021034) do
     t.string "name"
     t.string "typeImage"
     t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.decimal "price"
+    t.integer "stock"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
